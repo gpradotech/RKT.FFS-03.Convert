3221984210f107b2d40f5ce8f5395c9a4f27b22a
contato.gprado@gmail.com <contato.gprado@gmail.com>
2025-06-22 11:39:02 -0300

    corrige seleção de elementos do DOM e aprimora a validação de entrada no cálculo da conversão

--- v2/scripts.js
+++ v2/scripts.js
@@ -5,9 +5,9 @@ const input = document.querySelector('input');
 const select = document.querySelector('select');
 const form = document.querySelector('form');
 const button = document.querySelector('button');
-const footer = document.querySelector('footer');
-const caption = document.querySelector('footer span');
-const result = document.querySelector('footer h1');
+const footer = document.querySelector('main footer');
+const caption = document.querySelector('main footer span');
+const result = document.querySelector('main footer h1');
 
 const value = input.value;
 
@@ -36,7 +36,7 @@ const symbols = {
 // Trata o valor de input para aceitar apenas números e ponto
 input.addEventListener('input', function () {
   // Remove tudo que não for número ou ponto
-  let value = this.value.replace(/[^0-9.]/g, '');
+  let value = this.value.replace(/[^0-9.\,]/g, '').replace(',', '.');
   // Permite apenas um ponto
   const parts = value.split('.');
   if (parts.length > 2) {
@@ -52,21 +52,23 @@ function updateCaption() {
 
 function calculateConversion() {
   // Calcula o valor convertido
-  let calc = Number(input.value) * Number(conversionRates[select.value])
-  result.textContent = `R$ ${calc.toFixed(2).replace('.', ',')}`;
-
+  let calc = input.value * Number(conversionRates[select.value])
+  
   // Verifica se o valor é válido
-  if (isNaN(calc) || calc <= 0) {
-    footer.style.display = 'none';
+  if (isNaN(calc) || calc < 0) {
     alert('Por favor, insira um valor válido para conversão.');
+    footer.style.display = 'none'; // Exibe o resultado da conversão
   }
+
+  // Exibe o resultado da conversão
+  result.textContent = `R$ ${calc.toFixed(2).replace('.', ',')}`;
 }
 
 /////////////////////////////////////////////////////////////////
 
 
 form.onsubmit = (event) => {
-  event.preventDefault(); // Previne o comportamento padrão do botão
+  event.preventDefault(); // Previne o comportamento padrão do botã
   footer.style.display = 'block'; // Exibe o resultado da conversão
   updateCaption(); // Altera o texto da legenda conforme a moeda selecionada
   calculateConversion(); // Calcula a conversão inicial
